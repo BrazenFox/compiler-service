fc6fa023354925b396e360455f868a3f7f363063
dmma0521 <dmitrii.marokhonov@netcracker.com>
2023-04-27 16:26:51 +0300

    Add first code

--- internal/app/server/config.go
+++ internal/app/server/config.go
@@ -1,1 +1,13 @@
-package app
+package server
+
+type Config struct {
+	BindAddr string `toml:"bind_addr"`
+	LogLevel string `toml:"log_level"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BindAddr: ":8080",
+		LogLevel: "debug",
+	}
+}
